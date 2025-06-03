fc55f6ccfeed40c7335d5fa400cf7f8c519c2b62
Ulugbek0402 <udavlatboyev@gmail.com>
2025-06-03 19:46:53 +0500

    added admin_menu

--- main.py
+++ main.py
@@ -11,6 +11,34 @@ def user_menu():
 
 def admin_menu():
     "Ulug'bek"
+    print("""
+    1. Add category
+    2. Add product
+    3. See products
+    4. Delete product
+    5. See orders
+    6. Log out
+    """)
+
+    try:
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            return auth_menu()
+        else:
+            print("Invalid choice!")
+    except KeyboardInterrupt:
+        print("Good bye")
+    return admin_menu()
 
 
 if __name__ == '__main__':
