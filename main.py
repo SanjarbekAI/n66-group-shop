7f0c5c64467d1c17c36938f0140c9d364be6bf0e
Gavhar <saraah5778@gmail.com>
2025-06-03 19:56:27 +0500

    auth added

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from apps.utils.execute_tables import execute_table_queries
 
 
 def auth_menu():
-    "Gavhar"
+    print("welcome to the authentication menu!")
+    print(" 1 register")
+    print(" 2 login")
+    print(" 3 exit")
+    choice= input("Please choose an option: ")
+    if choice == '1':
+        pass
+        
+        
+    elif choice == '2':
+        pass
+ 
+    elif choice == '3':
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice, please try again.")
+
+
 
 
 def user_menu():
