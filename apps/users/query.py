d9be6b74b0a5680658020c3280f74361b86d95fa
Maxmud <maxmudbobomurodov151@gmail.com>
2025-06-03 19:40:42 +0500

    create user query

--- apps/users/query.py
+++ apps/users/query.py
@@ -1,2 +1,12 @@
 def create_user_table():
-    """Akobir"""
+
+    users = """
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    is_login BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    full_name VARCHAR(50) NOT NULL);
+    """
