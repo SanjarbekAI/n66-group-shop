59a53f1603b59f5d35ecaa8ffb79015a5406caac
SanjarbekAI <sanjarbekwork@gmail.com>
2025-06-03 20:26:31 +0500

    fixed

--- apps/products/query.py
+++ apps/products/query.py
@@ -1,51 +1,27 @@
-import psycopg2
+from core.database_settings import execute_query
+
 
 def create_category_table():
-    conn = psycopg2.connect(
-        dbname='postgres',
-        user='postgres',
-        password='1',
-        host='localhost',
-        port='5432'
-    )
-    cur = conn.cursor()
     query = """
-        CREATE TABLE IF NOT EXISTS categories (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL
-        );
-    """
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-    conn.close()
-    print("Categories table created.")
+            CREATE TABLE IF NOT EXISTS categories
+            (
+                id   SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL
+            );
+            """
+    execute_query(query=query)
+
 
 def create_product_table():
-    conn = psycopg2.connect(
-        dbname='postgres',
-        user='postgres',
-        password='1',
-        host='localhost',
-        port='5432'
-    )
-    cur = conn.cursor()
     query = """
-        CREATE TABLE IF NOT EXISTS products (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(150) NOT NULL,
-            category_id INTEGER REFERENCES categories(id),
-            quantity INTEGER NOT NULL,
-            price NUMERIC(10,2) NOT NULL,
-            added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-    """
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-    conn.close()
-    print("Products table created.")
-
-
-create_category_table()
-create_product_table()
+            CREATE TABLE IF NOT EXISTS products
+            (
+                id          SERIAL PRIMARY KEY,
+                name        VARCHAR(150)   NOT NULL,
+                category_id INTEGER REFERENCES categories (id),
+                quantity    INTEGER        NOT NULL,
+                price       NUMERIC(10, 2) NOT NULL,
+                added_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """
+    execute_query(query=query)
--- apps/utils/execute_tables.py
+++ apps/utils/execute_tables.py
@@ -1,2 +1,11 @@
+from apps.orders.query import create_order_table, create_order_item_table
+from apps.products.query import create_category_table, create_product_table
+from apps.users.query import create_user_table
+
+
 def execute_table_queries():
-    pass
+    create_user_table()
+    create_category_table()
+    create_product_table()
+    create_order_table()
+    create_order_item_table()
