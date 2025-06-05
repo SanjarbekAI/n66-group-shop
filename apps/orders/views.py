818c2e6ac2d3244969f752a2c5af072a27b2a68f
Akobir <akobirortiqov40@gmail.com>
2025-06-06 00:28:01 +0500

    fixing

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -4,7 +4,7 @@ def show_my_orders():
     id_query = """SELECT id FROM users WHERE is_login = TRUE;"""
     user_id = execute_query(query=id_query)
     print(user_id)
-    list_query = """SELECT i.product_id, i.price, i.quantity FROM order_item as INNER JOIN orders as o on o.id = i.order_id;"""
+    list_query = """SELECT i.product_id, i.price, i.quantity FROM order_item as i INNER JOIN orders as o on o.id = i.order_id;"""
     orders_list = execute_query(query=list_query, params=(user_id,), fetch="all")
 
     if not orders_list:
