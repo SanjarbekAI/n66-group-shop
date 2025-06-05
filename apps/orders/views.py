e89fa32521d57c71bf28a71a72fb4fbc27a103fb
Akobir <akobirortiqov40@gmail.com>
2025-06-05 18:42:02 +0500

    fix func show_my_orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,7 +3,7 @@ from core.database_settings import execute_query
 def show_my_orders():
     id_query = """SELECT id FROM users WHERE is_login = TRUE;"""
     res = execute_query(query=id_query)
-    user_id = res[0][0]
+    user_id = res
     list_query = """SELECT * FROM order_items WHERE order_id = %s;"""
     orders_list = execute_query(query=list_query, params=(user_id,), fetch="all")
 
